64ef0e53706462561de0c6ea593be95f400814a9
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 01:30:44 +0300

    Implement index & login view logic

--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
 
-# Create your views here.
+def index(request):
+    return render(request, 'landing/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index')
+        else:
+            context = {'error': 'Invalid email or password'}
+            return render(request, 'landing/login.html', context)
+    else:
+        return render(request, 'landing/login.html')
