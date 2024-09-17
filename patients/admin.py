6656f3a2e7900f0f95dbba9133ba11468b9d3597
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 14:17:43 +0300

    Register models into admin panel

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Patient, Visit, Appointment
 
-# Register your models here.
+admin.site.register(Patient)
+admin.site.register(Visit)
+admin.site.register(Appointment)
