2cb016b2df0fdae1bb1e7ca010e5f7397ae38077
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 08:44:52 +0300

    Implement returning all clinics view list

--- clinics/views.py
+++ clinics/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Clinic
 
-# Create your views here.
+@login_required
+def clinics_list(request):
+    clinics = Clinic.objects.all()
+    context = {
+        'clinics': clinics
+    }
+    return render(request, 'clinics/clinics_list.html', context)
+
