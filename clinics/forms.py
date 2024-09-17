758546d3466e07339a2456ca63e12a1b8a7678b8
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 09:08:13 +0300

    Modify file

--- clinics/forms.py
+++ clinics/forms.py
@@ -7,19 +7,3 @@ class ClinicForm(forms.ModelForm):
         model = Clinic
         fields = ['name', 'address', 'city', 'state', 'phone_number', 'email']
 
-class DoctorAffiliationForm(forms.ModelForm):
-    class Meta:
-        model = DoctorAffiliation
-        fields = ['doctor', 'office_address', 'working_days', 'working_hours']
-        widgets = {
-            'working_days': forms.CheckboxSelectMultiple(choices=[
-                ('Monday', 'Monday'),
-                ('Tuesday', 'Tuesday'),
-                ('Wednesday', 'Wednesday'),
-                ('Thursday', 'Thursday'),
-                ('Friday', 'Friday'),
-                ('Saturday', 'Saturday'),
-                ('Sunday', 'Sunday'),
-            ]),
-            'working_hours': forms.TextInput(attrs={'placeholder': '{"start": "09:00", "end": "17:00"}'}),
-        }
