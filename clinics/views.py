3624c6fefa0fe9bcdbc7ee705690e36a07dba7ad
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 09:05:47 +0300

    Implement clinic_detail view functionality

--- clinics/views.py
+++ clinics/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Clinic
+from .forms import ClinicForm
 
 @login_required
 def clinics_list(request):
@@ -9,4 +10,25 @@ def clinics_list(request):
         'clinics': clinics
     }
     return render(request, 'clinics/clinics_list.html', context)
+
+
+@login_required
+def clinic_detail(request, pk):
+    clinic = get_object_or_404(Clinic, pk=pk)
+    doctor_affiliations = clinic.doctor_affiliations.all()
+
+    if request.method == 'POST':
+        form = ClinicForm(request.POST, instance=clinic)
+        if form.is_valid():
+            form.save()
+            return redirect('clinic_detail', pk=clinic.pk)
+    else:
+        form = ClinicForm(instance=clinic)
+
+    context = {
+        'clinic': clinic,
+        'form': form,
+        'doctor_affiliations': doctor_affiliations
+    }
+    return render(request, 'clinics/clinic_detail.html', context)
 
