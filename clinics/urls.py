3a098665fb9cd2e74fbf848bae323f471265b064
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 13:00:12 +0300

    Create urls route for clinics views

---
+++ clinics/urls.py
@@ -0,0 +1,8 @@
+# clinics/urls.py
+from django.urls import path
+from .views import clinics_list, clinic_detail
+
+urlpatterns = [
+    path('', clinics_list, name='clinics'),
+    path('clinic/<int:pk>', clinic_detail, name='clinic'),
+]
