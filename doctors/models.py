6f6ce50cbf2052aef25842a4673cc7c877ab6d22
OthmanGh <othmanghandour51@gmail.com>
2024-09-17 14:20:08 +0300

    Modify specialties field in doctor model

--- doctors/models.py
+++ doctors/models.py
@@ -1,27 +1,21 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-
-SPECIALTIES = [
-    ('cleaning', 'Cleaning'),
-    ('filling', 'Filling'),
-    ('root_canal', 'Root Canal'),
-    ('crown', 'Crown'),
-    ('whitening', 'Teeth Whitening'),
-]
-
-class Specialty(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
+from multiselectfield import MultiSelectField
 
 class Doctor(models.Model):
+    SPECIALTY_CHOICES = (
+        ('cleaning', 'Cleaning'),
+        ('filling', 'Filling'),
+        ('root_canal', 'Root Canal'),
+        ('crown', 'Crown'),
+        ('teeth_whitening', 'Teeth Whitening'),
+    )
+
     npi = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
-    specialties = models.ManyToManyField(Specialty)
+    specialties = MultiSelectField(choices=SPECIALTY_CHOICES, default=[])
     clinics = models.ManyToManyField('clinics.Clinic', through='DoctorClinicAffiliation', related_name='doctors')
 
     def __str__(self):
