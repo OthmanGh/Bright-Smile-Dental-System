ba4bc95b5e3a04fbbaa7bb40f22ee3882d8ec080
OthmanGh <othmanghandour51@gmail.com>
2024-09-18 14:18:15 +0300

    Add doctors url paths

---
+++ doctors/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import doctors_list, doctor_detail
+
+urlpatterns = [
+    path('', doctors_list, name='doctors'),
+    path('doctor/<int:pk>/', doctor_detail, name='doctor'),
+
+]
