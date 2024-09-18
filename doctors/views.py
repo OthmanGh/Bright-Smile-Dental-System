b1ece447620cfc18e9b6789b08565971873a2c86
OthmanGh <othmanghandour51@gmail.com>
2024-09-18 14:22:56 +0300

    Implement single doctor page view logic

--- doctors/views.py
+++ doctors/views.py
@@ -9,3 +9,27 @@ def doctors_list(request):
     doctors = Doctor.objects.all()
     return render(request, 'doctors/doctors.html', {'doctors': doctors})
 
+
+@login_required
+def doctor_detail(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    
+    if request.method == 'POST':
+        form = DoctorForm(request.POST, instance=doctor)
+        if form.is_valid():
+            form.save()
+            return redirect('doctor', pk=doctor.pk)
+    else:
+        form = DoctorForm(instance=doctor)
+    
+    affiliations = DoctorClinicAffiliation.objects.filter(doctor=doctor)
+    patients = Patient.objects.filter(appointments__doctor=doctor).distinct()
+
+    context = {
+        'doctor': doctor,
+        'form': form,
+        'affiliations': affiliations,
+        'patients': patients,
+    }
+    
+    return render(request, 'doctors/doctor.html', context)
