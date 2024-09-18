8444e107dfdecce550dfbad3ad7ade7a2dd8d3e8
OthmanGh <othmanghandour51@gmail.com>
2024-09-18 14:17:34 +0300

    Implement returning doctors_list view logic

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Doctor, DoctorClinicAffiliation
+from .forms import DoctorForm
+from patients.models import Patient
 
-# Create your views here.
+@login_required
+def doctors_list(request):
+    doctors = Doctor.objects.all()
+    return render(request, 'doctors/doctors.html', {'doctors': doctors})
+
