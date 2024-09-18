f04da932a80a64f4f74a5f235027088801cdce2a
OthmanGh <othmanghandour51@gmail.com>
2024-09-18 14:16:39 +0300

    Include doctors url path

--- bright_smile_dental/urls.py
+++ bright_smile_dental/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
     path('clinics/', include('clinics.urls')),
+    path('doctors/', include('doctors.urls')),
 ]
