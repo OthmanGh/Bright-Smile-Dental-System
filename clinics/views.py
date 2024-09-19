06de456bcd35130f12b20f8f692b3b340f88d1fc
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 17:40:30 +0300

    Modify clinic detail view logic

--- clinics/views.py
+++ clinics/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Clinic
 from .forms import ClinicForm
+from doctors.models import DoctorClinicAffiliation, Doctor
 from doctors.forms import DoctorClinicAffiliationForm
 
 @login_required
@@ -16,40 +17,60 @@ def clinics_list(request):
 @login_required
 def clinic_detail(request, pk):
     clinic = get_object_or_404(Clinic, pk=pk)
-    doctor_affiliations = clinic.doctor_affiliations.all()
+
+    form = ClinicForm(instance=clinic)
+    affiliation_form = DoctorClinicAffiliationForm()
 
     if request.method == 'POST':
-        form = ClinicForm(request.POST, instance=clinic)
-        if form.is_valid():
-            form.save()
-            return redirect('clinics/clinic.html', pk=clinic.pk)
-    else:
-        form = ClinicForm(instance=clinic)
+        if 'clinic_form' in request.POST:
+            form = ClinicForm(request.POST, instance=clinic)
+            if form.is_valid():
+                form.save()
+                return redirect('clinic_detail', pk=clinic.pk)
+
+        elif 'affiliation_form' in request.POST:
+            affiliation_id = request.POST.get('affiliation_id')
+
+            if affiliation_id:
+                affiliation = get_object_or_404(DoctorClinicAffiliation, pk=affiliation_id)
+                form = DoctorClinicAffiliationForm(request.POST, instance=affiliation)
+            else:
+                form = DoctorClinicAffiliationForm(request.POST)
+                form.instance.clinic = clinic
+
+            if form.is_valid():
+                form.save()
+                return redirect('clinic_detail', pk=clinic.pk)
+            
+            else:
+                print(form.errors) 
+
+    affiliations = DoctorClinicAffiliation.objects.filter(clinic=clinic)
+
+    doctors = Doctor.objects.filter(affiliations__clinic=clinic).distinct()
+
+    affiliations_with_hours = []
+
+    for affiliation in affiliations:
+        working_hours = []
+        for day in affiliation.working_days:
+            hours = affiliation.working_hours.get(day, "")
+            working_hours.append(f"{day}: {hours}")
+
+        affiliations_with_hours.append({
+            'doctor': affiliation.doctor,
+            'office_address': affiliation.office_address,
+            'working_hours': ", ".join(working_hours),
+            'id': affiliation.pk
+        })
 
     context = {
         'clinic': clinic,
         'form': form,
-        'doctor_affiliations': doctor_affiliations
+        'affiliation_form': affiliation_form,
+        'affiliations_with_hours': affiliations_with_hours,
+        'doctors': doctors,  
     }
-    return render(request, 'clinics/clinic.html', context)
+    return render(request, 'clinics/clinic_detail.html', context)
 
 
-@login_required
-def add_doctor_affiliation(request, pk):
-    clinic = get_object_or_404(Clinic, pk=pk)
-    
-    if request.method == 'POST':
-        form = DoctorClinicAffiliationForm(request.POST)
-        if form.is_valid():
-            affiliation = form.save(commit=False)
-            affiliation.clinic = clinic
-            affiliation.save()
-            return redirect('clinic', pk=clinic.pk)
-    else:
-        form = DoctorClinicAffiliationForm()
-    
-    context = {
-        'clinic': clinic,
-        'form': form
-    }
-    return render(request, 'clinics/add_doctor_affiliation.html', context)
