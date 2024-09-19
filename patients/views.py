ceaca585b965640901d9abd78cb85218b6f6430e
OthmanGh <othmanghandour51@gmail.com>
2024-09-20 00:55:12 +0300

    Implement ability to add new visit for patient

--- patients/views.py
+++ patients/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Patient, Visit, Appointment
 from datetime import datetime
-from .forms import PatientForm
+from .forms import PatientForm, VisitForm
+from django.utils import timezone
+
 
 def patients_list(request):
     patients = Patient.objects.all()
@@ -35,24 +37,37 @@ def patients_list(request):
     return render(request, 'patients/patients_list.html', context)
 
 
+
 def patient_details(request, pk):
-    
     patient = get_object_or_404(Patient, pk=pk)
+
     form = PatientForm(instance=patient)
-    upcoming_appointments = patient.appointments.filter(appointment_date__gte=datetime.now())
+    
+    upcoming_appointments = patient.appointments.filter(appointment_date__gte=timezone.now())
     visits = Visit.objects.filter(patient=patient).select_related('doctor', 'clinic')
 
     if request.method == 'POST':
-        form = PatientForm(request.POST, instance=patient)
-        if form.is_valid():
-            form.save()
-            return redirect('patient', pk=patient.pk)
+        if 'save_patient' in request.POST:
+            form = PatientForm(request.POST, instance=patient)
+            if form.is_valid():
+                form.save()
+                return redirect('patient', pk=patient.pk)
+        elif 'add_visit' in request.POST:
+            visit_form = VisitForm(request.POST)
+            if visit_form.is_valid():
+                visit = visit_form.save(commit=False)
+                visit.patient = patient 
+                visit.save()
+                return redirect('patient', pk=patient.pk)
+    else:
+        visit_form = VisitForm() 
 
     context = {
         'patient': patient,
         'form': form,
+        'visit_form': visit_form,  
         'upcoming_appointments': upcoming_appointments,
-        'visits': visits
+        'visits': visits,
     }
 
     return render(request, 'patients/patient_detail.html', context)
