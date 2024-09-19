626f755e80bda72eeb712d193c2cdfc3eb313d5d
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 17:46:47 +0300

    Add open edit modal function

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,3 +1,10 @@
 function confirmSave() {
   return confirm('Are you sure you want to update this information?');
 }
+
+function openEditModal(affiliationId, doctorName, officeAddress, workingHours) {
+  document.getElementById('affiliation_id').value = affiliationId;
+  document.getElementById('doctor').value = doctorName;
+  document.getElementById('office_address').value = officeAddress;
+  document.getElementById('working_hours').value = workingHours;
+}
