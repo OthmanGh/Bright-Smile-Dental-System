0de5c22512648e702d9922e100f6015440f1bf85
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 21:31:04 +0300

    Add patient details page url path

--- patients/urls.py
+++ patients/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import patients_list
+from .views import patients_list, patient_details
 
 urlpatterns = [
-    path('', patients_list, name='patients' )
+    path('', patients_list, name='patients' ),
+    path('patient/<int:pk>', patient_details, name='patient'),
 ]
