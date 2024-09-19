08236fb5fdba083df6c46c8132ed7ef3a5fe5cb1
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 17:39:24 +0300

    Add fields to clinic from widgets

--- clinics/forms.py
+++ clinics/forms.py
@@ -4,5 +4,9 @@ from .models import Clinic
 class ClinicForm(forms.ModelForm):
     class Meta:
         model = Clinic
-        fields = ['name', 'address', 'city', 'state', 'phone_number', 'email']
-
+        fields = ['name', 'address', 'phone_number', 'email']
+        widgets = {
+            'address': forms.Textarea(attrs={'rows': 3}),
+            'phone_number': forms.TextInput(attrs={'maxlength': 15}),
+            'email': forms.EmailInput(attrs={'maxlength': 254}),
+        }
