904c7193c774d735c3e710cd14fca023992c7275
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 18:50:07 +0300

    Add last_visit & next_appointment fields to patients_list view

--- patients/views.py
+++ patients/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from .models import Patient
+from .models import Patient, Visit, Appointment
+from datetime import datetime
+
+from django.shortcuts import render
+from .models import Patient, Visit, Appointment
+from datetime import datetime
 
 def patients_list(request):
     patients = Patient.objects.all()
+
+    for patient in patients:
+        last_visit = Visit.objects.filter(patient=patient).order_by('-visit_date').first()
+
+        next_appointment = Appointment.objects.filter(patient=patient, appointment_date__gte=datetime.now()).order_by('appointment_date').first()
+
+        patient.last_visit = last_visit
+        patient.next_appointment = next_appointment
 
     context = {
         'patients': patients
     }
 
-    return render(request, 'patients/patients.html',  context)
+    return render(request, 'patients/patients.html', context)
+
