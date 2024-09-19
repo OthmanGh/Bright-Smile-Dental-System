f4113d2b385ac5930eed14fdbd2b414c8db9f437
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 18:41:00 +0300

    Modify clinic url path names

--- clinics/urls.py
+++ clinics/urls.py
@@ -3,5 +3,5 @@ from .views import clinics_list, clinic_detail
 
 urlpatterns = [
     path('', clinics_list, name='clinics'),
-    path('clinic/<int:pk>/', clinic_detail, name='clinic_detail'),
+    path('clinic/<int:pk>/', clinic_detail, name='clinic'),
 ]
