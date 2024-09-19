6e4ce8df46213a560d631cc5c75404f73d044ab1
OthmanGh <othmanghandour51@gmail.com>
2024-09-19 18:14:09 +0300

    Modify clinic affiliation form

--- doctors/forms.py
+++ doctors/forms.py
@@ -11,23 +11,10 @@ class DoctorForm(forms.ModelForm):
             'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
             'specialties': forms.CheckboxSelectMultiple,  
         }
-
+        
 
 
 class DoctorClinicAffiliationForm(forms.ModelForm):
     class Meta:
         model = DoctorClinicAffiliation
         fields = ['doctor', 'office_address', 'working_days', 'working_hours']
-        widgets = {
-            'working_days': forms.CheckboxSelectMultiple(choices=[
-                ('Monday', 'Monday'),
-                ('Tuesday', 'Tuesday'),
-                ('Wednesday', 'Wednesday'),
-                ('Thursday', 'Thursday'),
-                ('Friday', 'Friday'),
-                ('Saturday', 'Saturday'),
-                ('Sunday', 'Sunday'),
-            ]),
-            'working_hours': forms.TextInput(attrs={'placeholder': '{"start": "09:00", "end": "17:00"}'}),
-        }
-
