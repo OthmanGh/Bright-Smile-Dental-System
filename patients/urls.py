633fdc9c3d44a9c99e40135862dd787c2fa1ddcf
OthmanGh <othmanghandour51@gmail.com>
2024-09-20 14:38:29 +0300

    Add route for adding a new patient

--- patients/urls.py
+++ patients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import patients_list, patient_details
+from .views import patients_list, patient_details, add_patient
 
 urlpatterns = [
     path('', patients_list, name='patients' ),
     path('patient/<int:pk>', patient_details, name='patient'),
+    path('patients/', add_patient, name='add_patient'),
 ]
