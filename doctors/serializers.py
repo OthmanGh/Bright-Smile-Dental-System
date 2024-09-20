4d4f2602c2f9ecae79ece88e173d0c8197272590
OthmanGh <othmanghandour51@gmail.com>
2024-09-20 14:00:35 +0300

    Update DoctorSerializer to handle specialties as a list of choices

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Doctor
 
 class DoctorSerializer(serializers.ModelSerializer):
+    specialties = serializers.ListField(
+        child=serializers.ChoiceField(choices=[choice[0] for choice in Doctor.SPECIALTY_CHOICES]),
+        allow_empty=True
+    )
+
     class Meta:
         model = Doctor
         fields = '__all__'
