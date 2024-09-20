40af045d46796fca04d6702de7d6ff69fdd9ffec
OthmanGh <othmanghandour51@gmail.com>
2024-09-20 18:21:41 +0300

    Modify main url configuration

--- bright_smile_dental/urls.py
+++ bright_smile_dental/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('landing.urls')),
+    path('', include('landing.urls')),  
     path('clinics/', include('clinics.urls')),
-    path('doctors/', include('doctors.urls')),
-    path('patients/', include('patients.urls')),
-    path('api/', include('clinics.urls')),
-    path('api/', include('doctors.urls')),
-    path('api/', include('patients.urls')),
+    path('doctors/', include('doctors.urls')), 
+    path('patients/', include('patients.urls')), 
 
+    # API routes
+    path('api/clinics/', include('clinics.api_urls')),  
+    path('api/doctors/', include('doctors.api_urls')),  
+    path('api/patients/', include('patients.api_urls')),  
 ]
