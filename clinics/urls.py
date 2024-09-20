70c4234c5238dbeea500d4db860f2926a52136c5
OthmanGh <othmanghandour51@gmail.com>
2024-09-20 12:01:04 +0300

    Add get clinic route to urls path

--- clinics/urls.py
+++ clinics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import clinics_list, clinic_detail
+from .views import clinics_list, clinic_detail, get_clinic_info
 
 urlpatterns = [
     path('', clinics_list, name='clinics'),
-    path('clinic/<int:pk>/', clinic_detail, name='clinic'),
+    path('<int:pk>/', clinic_detail, name='clinic'),
+    path('clinics/<int:pk>/', get_clinic_info, name='clinic_info')
 ]
