3bbef95c216ec92edc64abe15f148556279a3e04
OthmanGh <othmanghandour51@gmail.com>
2024-09-21 22:59:05 +0300

    Comment local db variables

--- bright_smile_dental/settings.py
+++ bright_smile_dental/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'bright_smile_dental.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        # 'NAME': config('DB_NAME'),
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD'),
+        # 'HOST': config('DB_HOST'),
+        # 'PORT': config('DB_PORT'),
     }
 }
 
