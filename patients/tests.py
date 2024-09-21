c19624aa3dae76b3956806a3cb3ffdd7b54f59e6
OthmanGh <othmanghandour51@gmail.com>
2024-09-21 19:13:00 +0300

    Add unit tests for Patient model endpoint

--- patients/tests.py
+++ patients/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from .models import Patient
 
-# Create your tests here.
+class PatientTests(TestCase):
+    def test_add_patient(self):
+        url = reverse('add-patient')
+        data = {
+            "name": "Steve Havrd",
+            "date_of_birth": "2000-09-30",
+            "address": "123 street St",
+            "phone_number": "452-655-7426",
+            "last_4_ssn": "5724",
+            "gender": "Male"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Patient.objects.count(), 1) 
+        self.assertEqual(Patient.objects.last().name, 'Steve Havrd')
+
+    def test_add_patient_invalid_data(self):
+        url = reverse('add-patient')
+        data = {
+            "name": "Steve Havrd",
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
