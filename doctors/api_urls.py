292361975be7376f343d326e7317a843136c7db9
OthmanGh <othmanghandour51@gmail.com>
2024-09-21 19:35:28 +0300

    Remove extra url route

--- doctors/api_urls.py
+++ doctors/api_urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import add_doctor, doctor_detail
+from .views import add_doctor
 
 urlpatterns = [
     path('', add_doctor, name='add-doctor'), 
-    path('<int:pk>/', doctor_detail, name='doctor-info'),
 ]
